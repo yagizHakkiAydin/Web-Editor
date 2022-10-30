b18f5ab60b7902b46a02e7ec2567ba1111100e06
MFurkanYldz <87310054+MFurkanYldz@users.noreply.github.com>
2022-10-30 20:31:36 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let flyToggleButton = document.getElementById(".burger");
-let flyMenu = document.getElementById(".menu-tools");
-
-flyToggleButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  flyMenu.classList.toggle("open");
-});
